6d0985d04cb24b0b88ff8a1f305753dc7bff9893
delta <eetr3295@gmail.com>
2021-03-29 23:50:54 +0900

    20210329

--- CharacterControl.cs
+++ CharacterControl.cs
@@ -6,17 +6,14 @@ public class CharacterControl : MonoBehaviour
 {
 
     Rigidbody2D rb2d;
-    Collider2D col;
     Animator anim;
     Vector3 vel;
     Vector3 currentJumpForce;
 
-    // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
-        col = GetComponent<BoxCollider2D>();
     }
 
 
@@ -32,18 +29,8 @@ public class CharacterControl : MonoBehaviour
             vel = SetToBounce(rb2d.velocity); Debug.Log(rb2d.velocity);
             rb2d.AddForce(vel);
 
- //           rb2d.AddForce(new Vector3(-(currentJumpForce.x*0.3f ), currentJumpForce.y*0.3f,0));
         }
-        /*
-        else if (collision.gameObject.CompareTag("ceiling"))
-        {
-            Debug.Log(currentJumpForce);
-            anim.SetBool("Jump", true);
-            anim.SetBool("Idle", false);
-            anim.SetBool("Charge", false);
-            rb2d.AddForce(new Vector3((currentJumpForce.x*0.7f), 0, 0));
-        }
-        */
+
         else
         {
             anim.SetBool("Jump", false);
